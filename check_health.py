6303bf28485e796bbcac7c6e118a7a5443fdbefe
Garvit Kulshrestha <garvitindian@gmail.com>
2024-04-28 21:53:16 +0100

    Update check_health.py

--- check_health.py
+++ check_health.py
@@ -1,15 +1,23 @@
 import requests
-
-#Change it to your API endpoint.
-api_endpoint_url = "https://catfact.ninja/fact" 
-timeout = 2
+import smtplib
 
 """
 You can add your API key if your API endpoint requires the key for authentication.
 Technically API key is not required to just check the health of the API endpoint.
 Api key variable can be left blank as this will send the blank header which will not affect the health monitoring.
 """
-api_key = "" 
+api_endpoint_url = "https://catfact.ninja/fact"
+timeout = 2
+api_key = ""
+
+#You need to replace values of four below variables.
+sender_email = "your_sender_email@example.com"
+receiver_email = "your_receiver_email@example.com"
+smtp_username = "your_smtp_username"
+smtp_password = "your_smtp_password"
+
+smtp_server = "smtp.example.com"
+smtp_port = 587
 
 def check_health():
     try:
@@ -18,8 +26,16 @@ def check_health():
         if response.status_code == 200:
             print("Endpoint is healthy.")
         else:
-            print(f"Endpoint returned status code {response.status_code}")
+            send_email("API Health Check Failed", f"Endpoint returned status code {response.status_code}")
     except requests.exceptions.RequestException as e:
-        print(f"Failed to connect to endpoint {api_endpoint_url}. Error: {str(e)}")
+        send_email("API Health Check Failed", f"Failed to connect to endpoint {api_endpoint_url}. Error: {str(e)}")
+
+def send_email(subject, body):
+    message = f"Subject: {subject}\n\n{body}"
+
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        server.sendmail(sender_email, receiver_email, message)
 
 check_health()
